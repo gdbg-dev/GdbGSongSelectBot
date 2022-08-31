43520c1afa52ffcfe247ef6c5eb8478bfd3d46fd
gdbg <111021014+gdbg-dev@users.noreply.github.com>
2022-08-31 15:57:45 +0900

    型不一致エラーの修正

--- discordbot.py
+++ discordbot.py
@@ -29,7 +29,7 @@ async def Select(ctx):
 async def Select_in (ctx,year):
   if(ctx.message.channel.id==872096448387354685):
    URLCommonStr="https://gdbg.tv/release/"
-   albumnum=year-2009
+   albumnum=int(year)-2009
    albumlist=[[7,7,6,6,6],[17,17,5],[12,12,11],[12,12,13],[12,12,12],[10,9,9,10],[10,10,10,11],[10,10,10,10],[20,20],[6,5,5,6,5,5,5,5],[10,9,10,10,10],[12,12,12,12],[12,12,12,12]]
    thisalbum=albumlist[albumnum]
    albumsize=len(thisalbum)
