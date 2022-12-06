a824f69409485374a3cae6158387512f2c084615
gdbg <111021014+gdbg-dev@users.noreply.github.com>
2022-12-06 11:37:40 +0900

    typoです

--- discordbot.py
+++ discordbot.py
@@ -23,7 +23,7 @@ async def Select(ctx):
    albumlist=[[7,7,6,6,6],[17,17,5],[12,12,11],[12,12,13],[12,12,12],[10,9,9,10],[10,10,10,11],[10,10,10,10],[20,20],[6,5,5,6,5,5,5,5],[10,9,10,10,10],[12,12,12,12],[12,12,12,12]]
    thisalbum=albumlist[albumnum]
    albumsize=len(thisalbum)
-   discpos=random.randint(1,albumsie)#ディスクきめ
+   discpos=random.randint(1,albumsize)#ディスクきめ
    disc=thisalbum[discpos-1]
    track=random.randint(1,disc)
 
